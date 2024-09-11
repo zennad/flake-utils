e8a5a7ba2172cc268d144b1c56b419e303add1ab
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-09-11 19:37:38 +0200

    lib: eachSystem: reformat using 'nixfmt-rfc-style --width 80'
    Reformatting the eachSystem function using 'nixfmt-rfc-style --width 80'
    avoids formatting arguments in the following commits.

--- lib.nix
+++ lib.nix
@@ -28,31 +28,39 @@ let
 
   # Builds a map from <attr>=value to <attr>.<system>=value for each system
   #
-  eachSystem = systems: f:
+  eachSystem =
+    systems: f:
     let
       # Merge together the outputs for all systems.
-      op = attrs: system:
+      op =
+        attrs: system:
         let
           ret = f system;
-          op = attrs: key: attrs //
-              {
-                ${key} = (attrs.${key} or { })
-                  // { ${system} = ret.${key}; };
-              }
-          ;
+          op =
+            attrs: key:
+            attrs
+            // {
+              ${key} = (attrs.${key} or { }) // {
+                ${system} = ret.${key};
+              };
+            };
         in
         builtins.foldl' op attrs (builtins.attrNames ret);
     in
-    builtins.foldl' op { }
-      (systems
-       ++ # add the current system if --impure is used
-          (if builtins?currentSystem then
-             if builtins.elem builtins.currentSystem systems
-             then []
-             else [ builtins.currentSystem ]
-           else
-             []))
-  ;
+    builtins.foldl' op { } (
+      systems
+      ++
+        # add the current system if --impure is used
+        (
+          if builtins ? currentSystem then
+            if builtins.elem builtins.currentSystem systems then
+              [ ]
+            else
+              [ builtins.currentSystem ]
+          else
+            [ ]
+        )
+    );
 
   # eachSystemMap using defaultSystems
   eachDefaultSystemMap = eachSystemMap defaultSystems;
